9ddf9394bdfaac3b78ecbac0230b91e51b3f7257
MegaTilt <megatiltclub@gmail.com>
2017-06-30 13:15:45 +0800

    CAP to XCC

--- libraries/chain/db_init.cpp
+++ libraries/chain/db_init.cpp
@@ -449,8 +449,8 @@ void database::init_genesis(const genesis_state_type& genesis_state)
    const auto get_asset_id = [&assets_by_symbol](const string& symbol) {
       auto itr = assets_by_symbol.find(symbol);
 
-      // TODO: This is temporary for handling CAP snapshot
-      if( symbol == "CAP" )
+      // TODO: This is temporary for handling XCC snapshot
+      if( symbol == "XCC" )
           itr = assets_by_symbol.find(GRAPHENE_SYMBOL);
 
       FC_ASSERT(itr != assets_by_symbol.end(),
